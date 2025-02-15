513e05fe13df81d891e5f078a576a064d3e85e6c
Nithya-Darbha <ssanthoshi567@gmail.com>
2025-02-16 00:50:52 +0100

    Add files via upload

---
+++ HW3/hangrybirds.c
@@ -0,0 +1,85 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define WORMS 10
+#define BABYBIRDS 5
+int bowl = WORMS;
+int worms = WORMS;
+int numBbirds;
+int chirped = 0;
+
+sem_t mutex;
+sem_t emptyBowl;
+sem_t fullBowl;
+
+void *babyBird(void *arg) {
+    int birdId = (int)arg + 1;
+    while (1) {
+        sem_wait(&mutex);
+        if (bowl > 0) {
+            bowl--;
+            printf("bird %d eats a worm, bowl has %d worms left\n", birdId, bowl);
+            sem_post(&mutex);
+        } else {
+       
+        if (chirped == 0) {
+            chirped = 1;
+            printf("bird %d finds an empty bowl, chirps to refill\n", birdId);
+            sem_post(&emptyBowl);
+        }
+        sem_post(&mutex);
+        sem_wait(&fullBowl);
+        chirped = 0;
+        }
+        usleep(rand() % 100000);
+    }
+}
+
+
+void *parentBird(void *arg) {
+    while (1) {
+        sem_wait(&emptyBowl);
+        printf("parent bird refills the bowl with 10 worms\n");
+        sem_wait(&mutex);
+        bowl = WORMS;  
+        sem_post(&mutex); 
+        for (int i = 0; i < numBbirds; i++) {
+            sem_post(&fullBowl);
+        }
+        printf("bowl refilled with worms\n");
+        usleep(rand() % 200000);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("default values used\n");
+        numBbirds = BABYBIRDS;
+    } else {
+        numBbirds = atoi(argv[1]);
+    }
+    pthread_t birds[numBbirds];
+    pthread_t parent;
+    sem_init(&emptyBowl, 0, 0);
+    sem_init(&fullBowl, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+    pthread_create(&parent, NULL, parentBird, NULL);
+
+    for (int i = 0; i < numBbirds; i++) {
+        pthread_create(&birds[i], NULL, babyBird, (void *)i);
+    }
+
+    for (int i = 0; i < numBbirds; i++) {
+        pthread_join(birds[i], NULL);
+    }
+
+    pthread_join(parent, NULL);
+    sem_destroy(&emptyBowl);
+    sem_destroy(&fullBowl);
+    sem_destroy(&mutex);
+    return 0;
+}
